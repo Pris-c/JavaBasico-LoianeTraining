6593d9d2b2ab1a84ff5a30adc40377ec611a97ff
Pris-c <priscila.campoos@yahoo.com.br>
2023-05-18 21:43:06 -0300

    Começando com Java

---
+++ Começando_com_Java/2.Aula_PassandoArgumentosParaOPrograma/Argumentos.java
@@ -0,0 +1,11 @@
+class Argumentos{
+	
+	public static void main(String[] args){
+		
+		System.out.println("Você digitou " + args[0]);
+	}
+}
+
+
+//$ java Argumentos TextoSemEspaço
+//$ java Argumentos "Texto entre aspas"
---
+++ Começando_com_Java/3.Aula_EntendendoOsErros/Erro01.java
@@ -0,0 +1,16 @@
+class Erro01{
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello World!)
+		
+		//Correto:
+		//System.out.println("Hello World!");
+		
+	}
+}
+
+///Erro de sintaxe
+
+//*unclosed string literal
+                System.out.println("Hello World!)*/
